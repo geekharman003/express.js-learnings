253981672e11f50c7181f190400fa317a27e692a
geekharman003 <harmansadhra2003@gmail.com>
2025-12-05 20:41:56 +0530

    added the HTML view for products

---
+++ get-request-with-html/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const productRoutes = require("./routes/productRoutes");
+const PORT = 3000;
+
+app.use("/products", productRoutes);
+
+app.listen(PORT, ()=> {
+    console.log(`server is running on ${PORT}`)
+})
---
+++ get-request-with-html/controllers/productController.js
@@ -0,0 +1,6 @@
+const path = require("path");
+const getAllProducts = (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "views", "products.html"));
+};
+
+module.exports = { getAllProducts };
