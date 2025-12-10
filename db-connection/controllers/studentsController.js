83d5a2b7ee48aef643776d8dc37380e1ddec3859
geekharman003 <harmansadhra2003@gmail.com>
2025-12-10 14:28:00 +0530

    implemented the insertion,retrieving,updation,deletion api for students route

--- db-connection/controllers/studentsController.js
+++ db-connection/controllers/studentsController.js
@@ -1,10 +1,59 @@
 const db = require("../utils/db-connection");
 
+const getAllStudents = (req, res) => {
+  const fetchQuery = "SELECT * FROM students";
+  db.execute(fetchQuery, (err, result) => {
+    try {
+      if (err) {
+        res.status(500).send("error in fetching students details");
+        db.end();
+        throw new Error(err);
+      }
+
+      if (result.length === 0) {
+        res.status(404).send("no students found");
+        return;
+      }
+
+      res.status(200).send(result);
+    } catch (e) {
+      console.log(e.message);
+    }
+  });
+};
+
+const getStudentWithId = (req, res) => {
+  const { id } = req.params;
+  const fetchQuery = "SELECT * FROM students WHERE id = ?";
+  db.execute(fetchQuery, [id], (err, result) => {
+    try {
+      if (err) {
+        res.status(500).send("error fetching student details");
+        db.end();
+        throw new Error(err);
+      }
+
+      if (result.length === 0) {
+        res.status(404).send(`no student found with id ${id}`);
+        return;
+      }
+
+      res.status(200).send(result);
+    } catch (e) {
+      console.log(e.message);
+    }
+  });
+};
+
 const addEntry = (req, res) => {
-  const { name, email } = req.body;
-  const insertQuery = `INSERT INTO students (name,email)
-  VALUES (?,?)`;
-  db.execute(insertQuery, [name, email], (err) => {
+  const { name, email, age } = req.body;
+  if (!name || !email || !age) {
+    console.log("name, email and age is required");
+    return;
+  }
+  const insertQuery = `INSERT INTO students (name,email,age)
+  VALUES (?,?,?)`;
+  db.execute(insertQuery, [name, email, age], (err) => {
     try {
       if (err) {
         res.status(500).send("error in adding student into db");
@@ -13,16 +62,45 @@ const addEntry = (req, res) => {
       }
       res.status(200).send(`student with name ${name} added successfully`);
     } catch (e) {
-      console.log(e);
+      console.log(e.message);
     }
   });
 };
 
-const updateEntry = (req, res) => {
+function fetchOldData(id) {
+  const fetchQuery = "SELECT * FROM students WHERE id = ?";
+  return new Promise((resolve, reject) => {
+    try {
+      db.execute(fetchQuery, [id], (err, result) => {
+        if (err) {
+          return reject(err);
+        }
+
+        resolve(result[0]);
+      });
+    } catch (e) {
+      console.log(e.message);
+    }
+  });
+}
+
+const updateEntry = async (req, res) => {
   const { id } = req.params;
-  const { name, email } = req.body;
-  const updateQuery = "UPDATE students SET name = ?,email = ? WHERE id = ?";
-  db.execute(updateQuery, [name, email, id], (err, result) => {
+  const oldData = await fetchOldData(id);
+  let { name, email, age } = req.body;
+  if (!name) {
+    name = oldData["Name"];
+  }
+  if (!email) {
+    email = oldData["Email"];
+  }
+  if (!age) {
+    age = oldData["age"];
+  }
+
+  const updateQuery = `UPDATE students SET name = ?,email = ?,age = ? 
+  WHERE id = ?`;
+  db.execute(updateQuery, [name, email, age, id], (err, result) => {
     try {
       if (err) {
         res.status(500).send("error in updating student details");
@@ -61,9 +139,15 @@ const deleteEntry = (req, res) => {
 
       res.status(200).send(`student with id ${id} successfully deleted`);
     } catch (e) {
-      console.log(e);
+      console.log(e.message);
     }
   });
 };
 
-module.exports = { addEntry, updateEntry, deleteEntry };
+module.exports = {
+  getAllStudents,
+  getStudentWithId,
+  addEntry,
+  updateEntry,
+  deleteEntry,
+};
--- db-connection/routes/studentsRoutes.js
+++ db-connection/routes/studentsRoutes.js
@@ -2,8 +2,10 @@ const express = require("express");
 const studentController = require("../controllers/studentsController");
 const router = express.Router();
 
-router.post("/add", studentController.addEntry);
-router.put("/update/:id", studentController.updateEntry);
-router.delete("/delete/:id", studentController.deleteEntry);
+router.get("/", studentController.getAllStudents);
+router.get("/:id", studentController.getStudentWithId);
+router.post("/", studentController.addEntry);
+router.put("/:id", studentController.updateEntry);
+router.delete("/:id", studentController.deleteEntry);
 
 module.exports = router;
